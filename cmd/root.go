0444a1e23e29efe1b8e8bb3ba5339b5f926c309d
alfonmga <alfonmga@icloud.com>
2021-04-29 00:31:28 +0200

    Remove unused code

--- cmd/root.go
+++ cmd/root.go
@@ -15,27 +15,3 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
-
-/* func init() {
-cobra.OnInitialize(initConfig)
-rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mercadona-cli.yaml)")
-rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-} */
-
-/* func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".mercadona-cli")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
-} */
